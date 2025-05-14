f6a34489682eab56ec5b0197015844b0ebfd3671
Dennis Muga <dennis.mutethia1@accesscfa.com>
2025-05-14 23:29:47 +0300

    use neon db

--- utils/postgres_db.py
+++ utils/postgres_db.py
@@ -1,17 +1,8 @@
 import os, psycopg2
-from dotenv import load_dotenv
 
 class PostgresDb:
     def __init__(self):
-        # Load environment variables from .env file
-        load_dotenv()
-        self.conn_params = {
-            'host': os.getenv('DB_HOST'),
-            'database': os.getenv('DB_NAME'),
-            'port': os.getenv('DB_PORT'),
-            'user': os.getenv('DB_USER'),
-            'password': os.getenv('DB_PASSWORD')
-        }
+        self.database_url = os.getenv('DATABASE_URL')
         
         self.conn = None
         self.ensure_connection()
@@ -20,13 +11,14 @@ class PostgresDb:
         try:
             # Check if the connection is open
             if self.conn is None or self.conn.closed:
-                self.conn = psycopg2.connect(**self.conn_params)
+                self.conn = psycopg2.connect(self.database_url)
             else:
                 # Test the connection
                 with self.conn.cursor() as cursor:
                     cursor.execute("SELECT 1")
         except Exception as e:
+            print(e)
             # Reconnect if the connection is invalid
-            self.conn = psycopg2.connect(**self.conn_params)
+            self.conn = psycopg2.connect(self.database_url)
          
        
